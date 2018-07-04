4c07a79bb87a5cf7da1888f7c82d077f710e488c
Thomas Fahrer <fahrer92@gmail.com>
2018-07-04 11:36:42 +1000

    finisheed auth flow

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+if (process.env.NODE_ENV ==='production'){
+  //In production return prod keys
+  module.exports = require('./prod');
+
+}else{
+  //In development return dev keys
+module.exports = require('./dev');
+
+}
